15c33c28163947ab542e8f51d36c50dca6f845ad
FFucks <fabio_geiss@hotmail.com>
2026-01-16 09:59:51 -0300

    Arbitrary parameters code

--- src/main/scala/ArbitraryParameters.scala
+++ src/main/scala/ArbitraryParameters.scala
@@ -1,12 +1,20 @@
-
+import javax.annotation.processing.Messager
 
 object ArbitraryParameters {
 
-    enum Color:
-        case Red, Green, Blue
 
-    @main def run(xs: Int*): Unit =
-        println(s"The sum is ${xs.sum}")
+    /*@main def run(number: Int*): Unit =
+        println(s"The sum is ${number.sum}")*/
+
+    //The main method does not accept multiple parameter lists
+    @main def run(): Unit =
+        println(multipleParameters("The sum", 1, 2, 3, 4, 5))
+
+    //Arbitrary parameters needs to be the last parameter
+    def multipleParameters(message: String, numbers: Int*): String = {
+        s"${message} is ${numbers.sum}"
+    }
+
 
     /*@main def run(xs: Int*): Unit =
         println(s"Received ${xs.length} parameters: ${xs.mkString(", ")}")*/
